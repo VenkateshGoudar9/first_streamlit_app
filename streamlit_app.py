7e76deedb6bc6b9e4818d3fa1295d9c5d4cef7eb
VenkateshGoudar9 <118350897+VenkateshGoudar9@users.noreply.github.com>
2022-11-16 16:32:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,3 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
-FINAL Code we used for Zena’s Web Catalog P
